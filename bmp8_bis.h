60fcdbf440c35092cc5ad1f6e8c0bb047e787e13
coolclem <clem.koeniguer@gmail.com>
2025-03-21 12:23:33 +0100

    functions

---
+++ bmp8_bis.h
@@ -0,0 +1,19 @@
+# Role:
+
+# Role:
+# Parameter:
+# Result :
+typedef struct {
+    unsigned char header[54];
+    unsigned char colorTable[1024];
+    unsigned char * data;
+    unsigned int width;
+    unsigned int height;
+    unsigned int colorDepth;
+    unsigned int dataSize;
+} t_bmp8;
+
+t_bmp8 * bmp8_loadImage(const char * filename);
+void bmp8_saveImage(const char * filename, t_bmp8 * img);
+void bmp8_free(t_bmp8 * img);
+void bmp8_printInfo(t_bmp8 * img);
