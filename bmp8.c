7d2933382eb4f18cafb67c2ca7089622a5125755
laura.morenomaza <laura.moreno-maza@efrei.net>
2025-04-16 11:28:42 +0200

    Begin to write bmp8_saveImage() and bmp8_printInfo().

--- bmp8.c
+++ bmp8.c
@@ -1,13 +1,44 @@
 # Role:
 
 #include <stdio.h>
+#include <stdlib.h>
 
 # Role:
 # Parameter:
 # Result:
-void bmp8(t_bmp8 *filename) {
-    fopen (filename, "w");
+void bmp8_saveImage(const char * filename, t_bmp8 * img) {
+    //
+    FILE open = fopen (filename, "w");
+    if (open == NULL) {
+        printf ("Error opening file.\n");
+        return;
+    }
 
+    //
+    size_t write = fwrite(img,sizeof(t_bmp8), 1, filename);
+    if (write != 1) {
+        printf ("Error writing to file.\n");
+        return;
+    }
 
-    fclose (filename);
+    //
+    int close = fclose(filename);
+    if (close != 0) {
+        printf ("Error closing file.\n");
+        return;
+    }
+}
+
+void bmp8_printInfo(t_bmp8 * img) {
+    printf("Image Info:\n");
+    printf("\tWidth: %d\n\tHeight: %d\n", img->width, img->height);
+    printf("\tColor depth: %d\n\tData Size: %d\n", img->colorDepth, img->dataSize);
+}
+
+void bmp8_brightness(t_bmp8 * img, int value) {
+
+}
+
+void bmp8_applyFilter(t_bmp8 * img, float ** kernel, int kernelSize) {
+
 }
