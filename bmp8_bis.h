f70b5a1e494c370db27006a5adf9cd90d0c2ed51
coolclem <clem.koeniguer@gmail.com>
2025-05-02 11:18:03 +0200

    resolve error of 2 times same type

--- bmp8_bis.h
+++ bmp8_bis.h
@@ -3,15 +3,7 @@
 # Role:
 # Parameter:
 # Result :
-typedef struct {
-    unsigned char header[54];
-    unsigned char colorTable[1024];
-    unsigned char * data;
-    unsigned int width;
-    unsigned int height;
-    unsigned int colorDepth;
-    unsigned int dataSize;
-} t_bmp8;
+
 
 t_bmp8 * bmp8_loadImage(const char * filename);
 void bmp8_saveImage(const char * filename, t_bmp8 * img);
---
+++ main.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <bmp8.c>
+#include <bmp8_bis.c>
+
